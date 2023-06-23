bd8e0da135c62bce451dcfccefd3e285344c4931
des20 <des20@inf.ufpr.br>
2023-06-23 15:23:59 -0300

    usando a library

--- main.cpp
+++ main.cpp
@@ -1,42 +1,44 @@
-#include <graphviz/gvc.h>
-#include <graphviz/cgraph.h>
-#include <graphviz/cdt.h>
-#include <iostream>
-
-char visitado[]="visited";
-char verdadeiro[]="true";
-char vazio[]="";
-char falso[]="false";
-char inicio[]="A";
-void dfs(Agnode_t* node) {
-    agsafeset(node, visitado, verdadeiro, vazio);
-    std::cerr<<"Visited node: "<< agnameof(node)<<"\n";
-
-    Agedge_t* edge;
-    Agnode_t* neighbor;
-    for (edge = agfstedge(agroot(node), node); edge; edge = agnxtedge(agroot(node), edge, node)) {
-        neighbor = aghead(edge);
-        std::cerr<<"Neighbor name: " <<agnameof(neighbor)<<"\n";
-        if (!strcmp(agget(neighbor, visitado), falso)) {
-            dfs(neighbor);
-        }
-    }
-}
-
-
+#include "graphs.h"
+using namespace std;
 
 int main(){
+    vector<graphs> g;
     GVC_t* gvc = gvContext();
-
-    FILE* dotFile = fopen("./teste.dot", "r");
-    Agraph_t* graph = agread(dotFile, 0);
-
-    fclose(dotFile);
-    
-    Agnode_t* startNode = agnode(graph, inicio, 0);
-    if (startNode) {
-        dfs(startNode);
+    char filename[1024];
+    cin>>filename;
+    FILE* dotFile = fopen(filename, "r");
+    if (!dotFile){
+        std::cerr<<"File not found\n";
+        exit(1);
     }
+
+    Agraph_t* graph = agread(dotFile, 0);
+    std::fclose(dotFile);
+
+    Agnode_t* v = agfstnode(graph);
+    for (int i = 0; i < agnnodes(graph); i++){
+        graphs aux;
+        strncpy(aux.name,agnameof(v),1024);
+        aux.state=0;
+        g.push_back(aux);
+        v = agnxtnode(graph,v);
+    }
+    
+    for(int i = 0; i < agnnodes(graph); i++){
+        for(int j = 0; j < agnnodes(graph); j++){
+            if( agedge(graph, agnode(graph, g[i].name, 0), agnode(graph, g[j].name, 0), NULL, 0 ) ){
+                g[i].neighborhood.push_back(g[j]);
+            }
+        }
+    }
+
+    for(int i = 0; i < agnnodes(graph); i++){
+        for(int j = 0; j < g[i].neighborhood.size(); j++){
+            std::cerr<<g[i].name<<"->"<<g[i].neighborhood[j].name<<"\n";
+        }
+    }
+
+
 
 
     gvLayout(gvc, graph, "dot");
